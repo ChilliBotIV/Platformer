562db13767a3ccb36ab892e49e62ec55c226ae94
ChilliBotIV <tom.chachula@yorksj.ac.uk>
2024-05-10 15:23:43 +0100

    Drowning death sound and WHEELS!!!!
    Drown death sound and player has some fiery wheels.

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Respawn : MonoBehaviour
 {
     public float threshold;
+    public AudioSource drown;
 
     void FixedUpdate(){
         if(transform.position.y < threshold){
             transform.position = new Vector3(1, 2, 1);
+            drown.Play();
+
         }
     }
 }
